cab3dcaabe18eb02973e27fe4e73fe16a5daa972
Palesa <palesa.masiteng@gmail.com>
2023-07-30 20:30:30 +0200

    Updated JS and HTML  Fahrenheit conversion

--- src/app.js
+++ src/app.js
@@ -111,7 +111,18 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (14 * 9) / 5 + 32;
+  alert("fahrenheitTemperature");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Johannesburg");
+
+let fahrenheitLink = document.querySelector("fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
