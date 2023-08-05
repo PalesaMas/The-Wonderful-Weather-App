a69d56ddc4c5ead10a0217d6d9820a026e15ca9b
Palesa <palesa.masiteng@gmail.com>
2023-08-05 12:10:33 +0200

    Debug JS

--- src/app.js
+++ src/app.js
@@ -132,8 +132,8 @@ displayForecast();
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("fahrenheit-link");
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-let celsiusLink = document.querySelector("celsius-link");
+let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
